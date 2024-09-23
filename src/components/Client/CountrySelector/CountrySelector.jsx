850d07c97ffc3e66cf1985ebb32d2cee0b462972
Setanta Sports <144448083+setsports@users.noreply.github.com>
2024-09-23 18:34:21 +0400

    Update CountrySelector.jsx

--- src/components/Client/CountrySelector/CountrySelector.jsx
+++ src/components/Client/CountrySelector/CountrySelector.jsx
@@ -211,10 +211,9 @@ const CountrySelector = () => {
       {isPopupOpened ? (
         <CountryPopup setIsPopupOpened={setIsPopupOpened} />
       ) : localStorage.getItem('selectedPlatform') === 'APP' &&
-        !localStorage.getItem('appCountry') ? {
-          document.style.overflow = 'hidden';
-          return <CountryPopup setIsPopupOpened={setIsPopupOpened} />;
-      } : (
+        !localStorage.getItem('appCountry') ? (
+          <CountryPopup setIsPopupOpened={setIsPopupOpened} />;
+      ) : (
         ''
       )}
     </>
